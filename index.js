bd1bfda7c9b08b560dae41305e3465d115126f82
alumno <dragonxxxxzzzz@gmail.com>
2021-11-10 17:58:34 -0300

    primer commit

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const port = process.env.PORT || 3000;
+let usersConnected = 0;
 
+const port = process.env.PORT || 3000;
+const db = 'mongodb://localhost/lajeymonduela_chat'
+mongoose.connect(db);
+const MessageSchema = new mongoose.Schema({
+  nick: String, text: String
+});
+const Message = mongoose.model('Message', MessageSchema);
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  // DEBUG: console.log('a user connected');
+  console.log('a user connected');
+  usersConnected += 1;
+  console.log('usuarios conectados: ' + usersConnected);
   socket.on('chat message', (msg) => {
     // DEBUG: console.log(msg);
+    const message = new Message(msg);
     socket.broadcast.emit('chat message', msg);
+    message.save((err, msg) => console.log(msg));
   });
   socket.on('disconnect', () => {
-    // DEBUG: console.log('user disconnected');
+    console.log('user disconnected');
+    usersConnected -= 1;
+    console.log('usuarios conectados: ' + usersConnected);
   });
 });
 
--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -10,6 +10,8 @@ document.getElementById('form').addEventListener('submit', (e) => {
     nick: nick,
     text: message
   };
+  let timestamp = new Date();
+  console.log(timestamp.getHours(), timestamp.getMinutes());
   socket.emit('chat message', chatMessage);
   // clear input
   document.getElementById('msg').value = '';
@@ -17,31 +19,51 @@ document.getElementById('form').addEventListener('submit', (e) => {
   const div = document.createElement('div');
   const nickText = document.createElement('span');
   const messageText = document.createElement('span');
+  const timeText = document.createElement('span');
   nickText.classList.add('nick');
   nickText.innerHTML = nick;
   messageText.innerHTML = message;
+  timeText.innerHTML = timestamp.getHours() +':'+ timestamp.getMinutes()
   div.append(nickText);
   div.append(document.createElement('br'));
   div.append(messageText);
+  div.append(document.createElement('br'));
+  div.append(timeText);
+
   div.classList.add('sent-message');
   document.getElementById('messages').append(div);
   div.scrollIntoView();
   return false;
 });
 
-socket.on('chat message', (msg) => {
-  const div = document.createElement('div');
-  const nick = document.createElement('span');
-  const message = document.createElement('span');
-  nick.classList.add('nick');
-  nick.innerHTML = msg.nick;
-  message.innerHTML = msg.text;
-  div.append(nick);
-  div.append(document.createElement('br'));
-  div.append(message);
-  div.classList.add('chat-message');
-  document.getElementById('messages').append(div);
-  div.scrollIntoView();
-})
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if(h == 0){
+        h = 12;
+    }
+
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
+
+showTime();
 
 // TODO: agregar nicknames
